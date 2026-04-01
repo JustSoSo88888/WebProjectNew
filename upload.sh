0a5f1df506eb99dd2b65c1e09e710dce8d3d4440
super1231231 <superproject67@gmail.com>
2026-04-01 21:34:54 +0800

    1

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 
-npm run build && tar -zcvf dist.tar.gz .output/public  && scp -r ./dist.tar.gz jubaotangwschat:/www/wwwroot/www.novatravelexplore.com
+npm run generate && tar -zcvf dist.tar.gz .output/public  && scp -r ./dist.tar.gz jubaotangwschat:/www/wwwroot/www.novatravelexplore.com
 
 # 服务器上解压压缩文件
 # cd /www/wwwroot/www.novatravelexplore.com && tar -zxvf dist.tar.gz
