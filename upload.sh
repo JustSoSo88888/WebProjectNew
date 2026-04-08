503eaa8fca92b55539e5473828e885e4ecc5693c
super1231231 <superproject67@gmail.com>
2026-04-08 14:40:44 +0800

    1

--- upload.sh
+++ upload.sh
@@ -1,8 +1,6 @@
 
 npm run generate && tar -zcvf dist.tar.gz .output/public  && scp -r ./dist.tar.gz rztggiqs:/www/wwwroot/www.novatravelexplore.com
 
-#windows 文件压缩
-tar -zcvf dist.tar.gz -C .output/public .
 
 # 服务器上解压压缩文件
 # cd /www/wwwroot/www.novatravelexplore.com && tar -zxvf dist.tar.gz
