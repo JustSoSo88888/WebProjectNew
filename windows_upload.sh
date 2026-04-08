4c52486e4871933a06a62db7632b458380f5f2a4
super1231231 <superproject67@gmail.com>
2026-04-08 15:09:24 +0800

    1

--- windows_upload.sh
+++ windows_upload.sh
@@ -1,4 +1,4 @@
-npm run generate; tar -zcvf dist.tar.gz -C .output/public .
+npm run generate; tar -zcvf .output.tar.gz .output
 
 # 服务器上解压压缩文件
-# cd /www/wwwroot/www.novatravelexplore.com && tar -zxvf dist.tar.gz
+# cd /www/wwwroot/www.novatravelexplore.com && tar -zxvf .output.tar.gz
--- windows_upload_test.sh
+++ windows_upload_test.sh
@@ -1,4 +1,4 @@
-npm run generate; tar -zcvf dist.tar.gz -C .output/public .
+npm run generate; tar -zcvf .output.tar.gz .output
 
 # 服务器上解压压缩文件
-# cd /www/wwwroot/ntweb.xgdlandlink.shop && tar -zxvf dist.tar.gz
+# cd /www/wwwroot/ntweb.xgdlandlink.shop && tar -zxvf .output.tar.gz
